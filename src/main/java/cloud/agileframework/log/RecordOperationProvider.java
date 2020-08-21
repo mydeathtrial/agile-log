6738bfc56cfb4c78760a10954d0c2793044f88f2
mydeathtrial@163.com <mydeathtrial@163.com>
2020-08-21 18:46:50 +0800

    feat(代码移植): 根据请求获取请求的目标方法，移植到Spring-util中

--- src/main/java/cloud/agileframework/log/RecordOperationProvider.java
+++ src/main/java/cloud/agileframework/log/RecordOperationProvider.java
@@ -1,14 +1,9 @@
 package cloud.agileframework.log;
 
+import cloud.agileframework.spring.util.MappingUtil;
 import cloud.agileframework.spring.util.ServletUtil;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.HandlerExecutionChain;
-import org.springframework.web.servlet.handler.MatchableHandlerMapping;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Objects;
 
 /**
  * @author 佟盟
@@ -20,45 +15,14 @@ import java.util.Objects;
 public class RecordOperationProvider implements ExecutionObjectProvider {
 
     @Autowired
-    private ObjectProvider<MatchableHandlerMapping> handlerMappings;
-
-    @Autowired
     private RecordOperationManager recordOperationManager;
 
-    private static final String HANDLE_METHOD = "$cloud.agileframework.handlerMethod";
 
     @Override
     public void pass(ExecutionInfo executionInfo) {
-        HandlerMethod handlerMethod = identifying(ServletUtil.getCurrentRequest());
+        HandlerMethod handlerMethod = MappingUtil.matching(ServletUtil.getCurrentRequest());
 
         recordOperationManager.record(handlerMethod, executionInfo);
     }
 
-    /**
-     * 根据请求，提取请求的目标方法
-     *
-     * @param request 请求
-     * @return 目标方法标志
-     */
-    public HandlerMethod identifying(HttpServletRequest request) {
-        Object handlerMethod = request.getAttribute(HANDLE_METHOD);
-        if (handlerMethod instanceof HandlerMethod) {
-            return (HandlerMethod) handlerMethod;
-        }
-        return handlerMappings.orderedStream().map(handlerMapping -> {
-            try {
-                HandlerExecutionChain handlerExecutionChain = handlerMapping.getHandler(request);
-                Object handler;
-                if (handlerExecutionChain != null) {
-                    handler = handlerExecutionChain.getHandler();
-                    if (handler instanceof HandlerMethod) {
-                        return (HandlerMethod) handler;
-                    }
-                }
-            } catch (Exception ignored) {
-            }
-            return null;
-        }).filter(Objects::nonNull).findFirst().orElse(null);
-
-    }
 }
